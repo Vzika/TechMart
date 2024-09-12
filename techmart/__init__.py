7d886483bd697fadc24c6244a1c3501e7f2f2e88
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-09-12 13:07:19 +0100

    auth

--- techmart/__init__.py
+++ techmart/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 
@@ -11,7 +11,23 @@ def create_app():
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/techmart'
 	
 	
-	db.init_app(app)	
+	db.init_app(app)
+	
+	from .api import bp
+	from .auth import auth
+
+	app.register_blueprint(bp, url_prefix='/') #for route
+	app.register_blueprint(auth, url_prefix='/') # for auth
+ 
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
+ 
+	from .models import User
+
+	@login_manager.user_loader
+	def load_user(user_id):
+		return User.query.get(int(user_id))
 
 	return app
 
