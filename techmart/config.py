0f327eeac74ca9c1a560af2ad477659c6733cf64
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-17 21:35:38 +0100

    "Updated SECRET_KEY and SQLALCHEMY_DATABASE_URI in config.py"

--- techmart/config.py
+++ techmart/config.py
@@ -1,7 +1,8 @@
 import os
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/techmart'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URL') or 'mysql+pymysql://sql8731929:qiwYvQAVYT@sql8.freemysqlhosting.net/sql8731929'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
