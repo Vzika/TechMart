cbe744132e623ed6068099b1d958bf65d2cfed1a
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-17 21:34:22 +0100

    "Updated database URI in create_app function"

--- techmart/__init__.py
+++ techmart/__init__.py
@@ -4,29 +4,28 @@ from flask_login import LoginManager
 
 db = SQLAlchemy()
 
+
 def create_app():
-	app = Flask(__name__)
-	app.config['SECRET_KEY'] = 'zika1997'
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:admin@localhost/techmart'
-	
-	
-	db.init_app(app)
-	
-	from .api import bp
-	from .auth import auth
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'zika1997'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql8731929:qiwYvQAVYT@sql8.freemysqlhosting.net/sql8731929'
 
-	app.register_blueprint(bp, url_prefix='/') #for route
-	app.register_blueprint(auth, url_prefix='/') # for auth
- 
-	login_manager = LoginManager()
-	login_manager.login_view = 'auth.login'
-	login_manager.init_app(app)
- 
-	from .models import User
+    db.init_app(app)
 
-	@login_manager.user_loader
-	def load_user(user_id):
-		return User.query.get(int(user_id))
+    from .api import bp
+    from .auth import auth
 
-	return app
+    app.register_blueprint(bp, url_prefix='/')  # for route
+    app.register_blueprint(auth, url_prefix='/')  # for auth
 
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
