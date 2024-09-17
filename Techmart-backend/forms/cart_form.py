13d526337218d9d26a9a2069693dfa6d5fc52ca4
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-17 19:52:55 +0100

    cleaned out this branch and removes all conflicting files from other branches

--- Techmart-backend/forms/cart_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
-
-class CartForm(FlaskForm):
-    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1)])
-    submit = SubmitField('Update Cart')
-
