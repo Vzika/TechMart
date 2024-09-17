3b6f0e0b7416757b920a915932ff0726eb8e8f06
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-17 18:40:23 +0100

    No files should be in the main branch until we are ready to merge

--- Techmart-backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/techmart'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
--- Techmart-backend/forms/review_form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import IntegerField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-class ReviewForm(FlaskForm):
-    rating = IntegerField('Rating', validators=[DataRequired()])
-    comment = TextAreaField('Comment')
-    submit = SubmitField('Submit')
-
