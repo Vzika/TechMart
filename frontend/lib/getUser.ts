13deaf0ee7752f2a1ce09d531f345cbffd36e79d
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-19 00:06:01 +0100

    Change the backend API url on productserver.tsx

--- frontend/lib/getUser.ts
+++ frontend/lib/getUser.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = `http://127.0.0.1:5000`;
+const API_URI = `https://techmart-y7g6.onrender.com`;
 
 export const getUserData = async () => {
   if (typeof window !== "undefined") {
