298007f1087b12e81fd18eaac0fa13c35e859958
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-19 01:52:48 +0100

    confirming the authentication routes

--- techmart/models/models.py
+++ techmart/models/models.py
@@ -6,16 +6,19 @@ from . import db
 # User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    orders = db.relationship('Order', backref='user', lazy=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    # orders = db.relationship('Order', backref='user', lazy=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 
 # Product Model
@@ -24,12 +27,19 @@ class Product(db.Model):
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Float, nullable=False)
-    stock = db.Column(db.Integer, default=0)
+    quantity = db.Column(db.Integer, default=1)
+    # orders = db.relationship('Order', backref='user',
+    #                          lazy=True)  # incase of debugging
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
 
 
 # Order Model
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    product_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, nullable=False)
+    # product_id = db.Column(
+    #     db.Integer, db.ForeignKey('product.id'), nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)  # incase of debugging
     created_at = db.Column(db.DateTime, default=datetime.now)
