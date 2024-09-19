277178598435622bd0391cf4a4a532576c8421e5
The Primotion Studio <oyedelenewton@gmail.com>
2024-09-19 01:31:43 +0100

    "Refactored __init__.py: removed imports, reorganized code, and updated configurations"

--- techmart/__init__.py
+++ techmart/__init__.py
@@ -1,41 +1,29 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify
+from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-
-db = SQLAlchemy()
+from models import db
+from routes import register_routes
 
 
 def create_app():
     app = Flask(__name__)
+
     CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
-    app.config['SECRET_KEY'] = 'zika1997'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql8731929:qiwYvQAVYT@sql8.freemysqlhosting.net/sql8731929'
+    app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
 
+    # Initialize extensions
     db.init_app(app)
+    jwt = JWTManager(app)
 
-    from .routes.api import api
-    from .routes.auth import auth
-
-    app.register_blueprint(api, url_prefix='/')  # for route
-    app.register_blueprint(auth, url_prefix='/')  # for auth
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    from .models import User
+    # Register Routes
+    register_routes(app)
 
     @app.route('/', methods=['GET'])
     def welcome():
         return jsonify({'message': 'Welcome to TechMart'})
-
-    @login_manager.unauthorized_handler
-    def unauthorized():
-        return jsonify({"error": "Unauthorized access. Please log in."}), 401
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     return app
