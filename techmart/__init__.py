f2941d28db4eb838103f2e9e2a7704795b575094
The Primotion Studio (Prime) <88973898+PrimotionStudio@users.noreply.github.com>
2024-09-19 04:12:40 +0100

    enabled CORS for all origins

--- techmart/__init__.py
+++ techmart/__init__.py
@@ -10,7 +10,8 @@ from techmart.routes import register_routes
 def create_app():
     app = Flask(__name__)
 
-    CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+    # CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+    CORS(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:user@localhost/techmart'
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql8731929:qiwYvQAVYT@sql8.freemysqlhosting.net/sql8731929'
     app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
